a921103720e264fcc37d26ae62c9c3fcf43b13aa
Arun Venkatraman <arunvenk@cs.cmu.edu>
2017-01-06 00:19:30 -0500

    hindsight ilqr with value function computation

---
+++ src/templated/iLQR_hindsight_value.hh
@@ -0,0 +1,143 @@
+// 
+// Implements iLQR (on a traditional chain) for nonlinear dynamics and cost.
+//
+// Arun Venkatraman (arunvenk@cs.cmu.edu)
+// December 2016
+//
+
+#pragma once
+
+
+#include <templated/taylor_expansion.hh>
+#include <utils/debug_utils.hh>
+
+#include <Eigen/Dense>
+
+#include <functional>
+#include <vector>
+
+namespace ilqr
+{
+
+// Defined in templated/taylor_expansion.hh
+//template<int _rows, int _cols>
+//using Matrix = Eigen::Matrix<double, _rows, _cols>;
+//
+//template<int _rows>
+//using Vector = Eigen::Matrix<double, _rows, 1>;
+
+template<int xdim, int udim>
+struct HindsightBranchValue
+{
+    using Dynamics = std::function<Vector<xdim>(const Vector<xdim> &x, const Vector<udim> &u)>;
+    using Cost = std::function<double(const Vector<xdim> &x, const Vector<udim> &u, const int t)>;
+    using FinalCost = std::function<double(const Vector<xdim> &x)>;
+
+    HindsightBranchValue(const Dynamics &dyn, const FinalCost &cost_final, const Cost &cost_regular, const double prob)
+        : dynamics(dyn), final_cost(cost_final), cost(cost_regular), probability(prob)
+    {
+    }
+
+    Dynamics dynamics;
+    FinalCost final_cost;
+    Cost cost;
+
+    double probability = 0;
+
+    // Feedback control gains.
+    std::vector<Matrix<udim, xdim>> Ks;
+    std::vector<Vector<udim>> ks;
+
+    // Linearization points.
+    std::vector<Vector<xdim>> xhat;
+    std::vector<Vector<udim>> uhat;
+
+    // Value function terms.
+    std::vector<Matrix<xdim, xdim>> Vs;
+    std::vector<Matrix<1, xdim>> Gs;
+    std::vector<double> Ws;
+
+};
+
+template<int xdim, int udim>
+class iLQRHindsightValueSolver
+{
+    static_assert(xdim > 0, "State dimension should be greater than 0");
+    static_assert(udim > 0, "Control dimension should be greater than 0");
+public:
+    using Dynamics = std::function<Vector<xdim>(const Vector<xdim> &x, const Vector<udim> &u)>;
+    using Cost = std::function<double(const Vector<xdim> &x, const Vector<udim> &u, const int t)>;
+    using FinalCost = std::function<double(const Vector<xdim> &x)>;
+
+    iLQRHindsightValueSolver(const std::vector<HindsightBranchValue<xdim,udim>> &branches)
+    {
+        IS_GREATER(branches.size(), 0);
+        branches_ = branches; 
+        IS_ALMOST_EQUAL(total_branch_probability(), 1.0, 1e-3);
+    }
+
+    // Computes the control at timestep 0 using K0_, k0_ that is
+    // shared across all branches.
+    inline Vector<udim> compute_first_control(const Vector<xdim> &x0) const;
+
+    // Computes the control at timestep t at xt.
+    // :param alpha - Backtracking line search parameter. 
+    //      Setting to 1 gives regular forward pass.
+    inline Vector<udim> compute_control_stepsize(const int branch_num, const Vector<xdim> &xt, const int t, const double alpha) const;
+
+    // :param alpha - Backtracking line search parameter. 
+    //      Setting to 1 gives regular forward pass.
+    inline double forward_pass(const int branch_num,
+            const Vector<xdim> x_init, 
+            std::vector<Vector<xdim>> &states, std::vector<Vector<udim>> &controls, 
+            const double alpha ) const;
+
+
+    // :param x_init - Initial state from which to start the system from.
+    // :param u_nominal - Initial control used for the whole sequence during 
+    //      the first forward pass.
+    // :param mu - Levenberg-Marquardt parameter for damping the least-squares. 
+    //      Setting it to 0 gets the default behavior. The damping makes the 
+    //      state-space steps smaller over iterations. 
+    inline void solve(const int T, const Vector<xdim> &x_init, 
+            const Vector<udim> u_nominal, const double mu, 
+            const int max_iters = 1000, bool verbose = false, 
+            const double cost_convg_ratio = 1e-4, const double start_alpha = 1.0, const bool store_value_fnc = false,
+            const bool warm_start = false, const int t_offset = 0);
+
+    // Returns how many timesteps we have computed control policies for.
+    inline int timesteps() const;
+
+    // Set the probability of a branch. Allows it to be done in place so  
+    // warm start solving can be used.
+    inline void set_branch_probability(const int branch_num, const double probability);
+
+    inline double compute_value(const int branch_num, const Vector<xdim> &xt, const int t) const;
+
+private:
+    std::vector<HindsightBranchValue<xdim,udim>> branches_;
+
+    // Feedback control gains.
+    Matrix<udim, xdim> K0_ = Matrix<udim, xdim>::Zero();
+    Vector<udim> k0_ = Vector<udim>::Zero();
+
+    // Linearization points for the first timestep.
+    Vector<xdim> xhat0_ = Vector<xdim>::Zero();
+    Vector<udim> uhat0_ = Vector<udim>::Zero();
+
+    // Performs one timestep of the bellman backup.
+    // :param t - passed to the cost runction
+    // :param mu - Levenberg-Marquardt parameter
+    inline void bellman_backup(const int branch_num, const int t, 
+        const double mu, 
+        const Matrix<xdim,xdim> &Vt1, const Matrix<1,xdim> &Gt1, const double Wt1,
+        Matrix<xdim,xdim> &Vt, Matrix<1,xdim> &Gt, double &Wt);
+
+    inline double total_branch_probability();
+
+};
+
+} // namespace lqr
+
+#include <templated/iLQR_hindsight_value_impl.hh>
+
---
+++ src/templated/iLQR_hindsight_value_impl.hh
@@ -0,0 +1,434 @@
+// 
+// Implements iLQR (on a traditional chain) for nonlinear dynamics and cost.
+//
+// Arun Venkatraman (arunvenk@cs.cmu.edu)
+// December 2016
+//
+
+#pragma once
+
+#include <templated/iLQR_hindsight_value.hh>
+
+#include <ostream>
+
+namespace ilqr
+{
+
+template<int xdim, int udim>
+inline Vector<udim> iLQRHindsightValueSolver<xdim,udim>::compute_first_control(const Vector<xdim> &x0) const
+{    
+    const Vector<xdim> zt = (x0 - xhat0_);
+    const Vector<udim> vt = K0_ * zt + k0_;
+
+    return Vector<udim>(vt + uhat0_);
+}
+
+// Computes the control at timestep t at xt.
+// :param alpha - Backtracking line search parameter. Setting to 1 gives regular forward pass.
+template<int xdim, int udim>
+inline Vector<udim> iLQRHindsightValueSolver<xdim,udim>::compute_control_stepsize(
+        const int branch_num, const Vector<xdim> &xt, const int t, const double alpha) const
+{
+    IS_BETWEEN_LOWER_INCLUSIVE(branch_num, 0, branches_.size());
+    const HindsightBranchValue<xdim,udim> &branch = branches_[branch_num];
+
+    const Matrix<udim, xdim> &Kt = branch.Ks[t];
+    const Vector<udim> &kt = branch.ks[t];
+
+    const Vector<xdim> zt = (xt - branch.xhat[t]);
+    const Vector<udim> vt = Kt * zt + alpha*kt;
+
+    return Vector<udim>(vt + branch.uhat[t]);
+}
+
+template<int xdim, int udim>
+inline double iLQRHindsightValueSolver<xdim,udim>::forward_pass(const int branch_num,
+            const Vector<xdim> x_init,  
+            std::vector<Vector<xdim>> &states,
+            std::vector<Vector<udim>> &controls,
+            const double alpha
+        ) const
+{
+    IS_BETWEEN_LOWER_INCLUSIVE(branch_num, 0, branches_.size());
+
+    const int T = timesteps();
+
+    const HindsightBranchValue<xdim,udim> &branch = branches_[branch_num];
+    const auto &dynamics_fnc = branch.dynamics;
+    const auto &cost_fnc = branch.cost;
+    const auto &final_cost_fnc = branch.final_cost;
+
+    controls.resize(T);
+    states.resize(T+1);
+
+    states[0] = x_init;
+    double cost_to_go = 0;
+    for (int t = 0; t < T; ++t)
+    {
+        controls[t] = compute_control_stepsize(branch_num, states[t], t, alpha); 
+
+        const double cost = cost_fnc(states[t], controls[t], t);
+        cost_to_go += cost;
+
+        // Roll forward the dynamics.
+        const Vector<xdim> xt1 = dynamics_fnc(states[t], controls[t]);
+        states[t+1] = xt1;
+    }
+    const double final_cost = final_cost_fnc(states[T]); 
+    cost_to_go += final_cost;
+
+    return cost_to_go;
+}
+
+template<int xdim, int udim>
+inline void iLQRHindsightValueSolver<xdim,udim>::solve(const int T, 
+        const Vector<xdim> &x_init, const Vector<udim> u_nominal, 
+        const double mu, const int max_iters, bool verbose, 
+        const double cost_convg_ratio, const double start_alpha,
+        const bool store_value_fnc, const bool warm_start, const int t_offset)
+{
+    IS_GREATER(T, 1);
+    IS_GREATER_EQUAL(mu, 0);
+    IS_GREATER(max_iters, 0);
+    IS_GREATER(cost_convg_ratio, 0);
+    IS_GREATER(start_alpha, 0);
+    IS_GREATER_EQUAL(t_offset, 0);
+
+    // Check that the branch probabilities sum to 1.
+    IS_ALMOST_EQUAL(total_branch_probability(), 1.0, 1e-3);
+
+    const int num_branches = branches_.size(); 
+    // If we are not doing a warm start, initialiew all the variables.
+    if (!warm_start)
+    {
+        xhat0_ = Vector<xdim>::Zero();
+        uhat0_ = u_nominal;
+        K0_ = Matrix<udim,xdim>::Zero();
+        k0_ = Vector<udim>::Zero();
+
+        // Initialize each branch
+        for (int branch_num = 0; branch_num < num_branches; ++branch_num)
+        {
+            HindsightBranchValue<xdim,udim> &branch = branches_[branch_num];
+            branch.Ks = std::vector<Matrix<udim, xdim>>(T, Matrix<udim, xdim>::Zero());
+            branch.ks = std::vector<Vector<udim>>(T, Vector<udim>::Zero());
+
+            branch.uhat = std::vector<Vector<udim>>(T, u_nominal);
+            branch.xhat = std::vector<Vector<xdim>>(T+1, Vector<xdim>::Zero());
+
+            branch.Vs = std::vector<Matrix<xdim, xdim>>(T+1, Matrix<xdim, xdim>::Zero());
+            branch.Gs = std::vector<Matrix<1, xdim>>(T+1, Matrix<1, xdim>::Zero());
+            branch.Ws = std::vector<double>(T+1, 0.0);
+        }
+    }
+    else // if we warm start
+    {
+        // Since we need the first timestep to use the same x0, u0, K0, k0
+        // we can compute an average for K0 and k0 and set x0, u0 to x_init
+        // and u_nominal.
+        xhat0_ = x_init;
+        uhat0_ = u_nominal;
+        K0_.setZero(); k0_.setZero();
+
+        // Resize based on t_offset to make sure sizes are correct for variables.
+        for (int branch_num = 0; branch_num < num_branches; ++branch_num)
+        {
+            HindsightBranchValue<xdim,udim> &branch = branches_[branch_num];
+            const int old_size = branch.Ks.size();
+            IS_GREATER(old_size, t_offset);
+            branch.Ks.erase(branch.Ks.begin(), branch.Ks.begin()+t_offset);
+            branch.ks.erase(branch.ks.begin(), branch.ks.begin()+t_offset);
+            branch.uhat.erase(branch.uhat.begin(), branch.uhat.begin()+t_offset);
+            branch.xhat.erase(branch.xhat.begin(), branch.xhat.begin()+t_offset);
+
+            // Confirm that the time horizon matches the size of requried
+            // variables. 
+            IS_EQUAL(branch.Ks.size(), T);
+            IS_EQUAL(branch.ks.size(), T);
+            IS_EQUAL(branch.uhat.size(), T);
+            IS_EQUAL(branch.xhat.size(), T+1);
+
+            K0_ += branch.probability * branch.Ks.at(0);
+            k0_ += branch.probability * branch.ks.at(0);
+        }
+
+        for (int branch_num = 0; branch_num < num_branches; ++branch_num)
+        {
+            HindsightBranchValue<xdim,udim> &branch = branches_[branch_num];
+            branch.Ks.at(0) = K0_;
+            branch.ks.at(0) = k0_;
+            branch.xhat.at(0) = xhat0_;
+            branch.uhat.at(0) = uhat0_;
+        }
+    }
+
+    // Holders used for the line search.
+    std::vector<Vector<udim>> uhat_new (T, Vector<udim>::Zero());
+    std::vector<Vector<xdim>> xhat_new(T+1, Vector<xdim>::Zero());
+
+    double old_cost = std::numeric_limits<double>::infinity();
+
+    int iter = 0;
+    for (iter = 0; iter < max_iters; ++iter)
+    {
+        // Line search as decribed in 
+        // http://homes.cs.washington.edu/~todorov/papers/TassaIROS12.pdf
+        
+        // Initial step-size
+        double alpha = start_alpha;
+
+        // The step-size adaptation paramter
+        constexpr double beta = 0.5; 
+        static_assert(beta > 0.0 && beta < 1.0, 
+            "Step-size adaptation parameter should decrease the step-size");
+        
+        // Before we start line search, NaN makes sure termination conditions 
+        // won't be met.
+        double new_cost = std::numeric_limits<double>::quiet_NaN();
+        double cost_diff_ratio = std::abs((old_cost - new_cost) / new_cost);
+
+        while(!(new_cost < old_cost || cost_diff_ratio < cost_convg_ratio))
+        {
+            new_cost = 0;
+            for (int branch_num = 0; branch_num < num_branches; ++branch_num)
+            {
+                const double branch_new_cost 
+                    = forward_pass(branch_num, x_init, xhat_new, uhat_new, alpha);
+                const double p = branches_[branch_num].probability;
+                new_cost +=  p * branch_new_cost;
+            }
+            cost_diff_ratio = std::abs((old_cost - new_cost) / new_cost);
+
+            // Try decreasing the step-size by beta-times. 
+            alpha *= beta;
+        } 
+
+        // Since we always beta-it at the end of the iteration, invert it to get 
+        // the step-size that worked.
+        alpha = (1.0/beta)*alpha;
+
+        // For each branch with this step size, 
+        std::vector<Vector<xdim>> x0s(num_branches);
+        std::vector<Vector<udim>> u0s(num_branches);
+        for (int branch_num = 0; branch_num < num_branches; ++branch_num)
+        {
+            forward_pass(branch_num, x_init, xhat_new, uhat_new, alpha);
+            x0s[branch_num] = xhat_new[0];
+            u0s[branch_num] = uhat_new[0];
+
+            branches_[branch_num].xhat.swap(xhat_new);
+            branches_[branch_num].uhat.swap(uhat_new);
+        }
+        //TODO If they are equal across all branches, we can just grab one 
+        // instead of storing them all.
+        xhat0_ = x0s[0];
+        uhat0_ = u0s[0];
+
+        if (verbose)
+        {
+            PRINT("[Iter " << iter << "]: Alpha: " << alpha 
+                    << ", Cost ratio: " << cost_diff_ratio 
+                    << ", New Cost: " << new_cost
+                    << ", Old Cost: " << old_cost);
+        }
+
+        old_cost = new_cost;
+
+        if (cost_diff_ratio < cost_convg_ratio) 
+        {
+            break;
+        }
+
+        // Backup each branch separately.
+        for (int branch_num = 0; branch_num < num_branches; ++branch_num)
+        {
+            const HindsightBranchValue<xdim,udim> &branch = branches_[branch_num];
+            quadratize_cost(branch.final_cost, branch.xhat.back(), branch.Vs.at(T), branch.Gs.at(T));
+            branch.Ws.at(T) = branch.final_cost(branch.xhat.back());
+
+            // Storage for backing up the value function.
+            Matrix<xdim, xdim> Vt; 
+            Matrix<1, xdim> Gt;
+
+            // Backwards pass for this branch. 
+            // We do it until t > =0 for shared autonomy vs for regular
+            // hindsight.
+            // timestep separately.
+            for (int t = T-1; t >= 0; --t)
+            {
+                bellman_backup(branch_num, t, mu, branch.Vs.at(t+1), branch.Gs.at(t+1), branch.Ws.at(t+1),
+                        branch.Vs.at(t), branch.Vs.at(t), branch.Ws.at(t));
+            }
+        }
+
+        // Now merge the branches at the top timestep.
+        Matrix<udim,udim> weighted_inv_term; weighted_inv_term.setZero();
+        Matrix<udim,xdim> weighted_Kt_term; weighted_Kt_term.setZero();
+        Vector<udim> weighted_kt_term; weighted_kt_term.setZero();
+
+        // Levenberg-Marquardt parameter for damping. 
+        // ie. eigenvalue inflation matrix.
+        const Matrix<xdim, xdim> LM = mu * Matrix<xdim, xdim>::Identity();
+
+        // w stands for weighted by probability of each branch.
+        Matrix<xdim,xdim> wQ; wQ.setZero();
+        Matrix<udim,udim> wR; wR.setZero();
+        Matrix<xdim,udim> wP; wP.setZero();
+        Vector<xdim> wg_x; wg_x.setZero();
+        Vector<udim> wg_u; wg_u.setZero();
+        for (int branch_num = 0; branch_num < num_branches; ++branch_num)
+        {
+            const HindsightBranchValue<xdim,udim> &branch = branches_[branch_num];
+            const double p = branch.probability;
+
+            // Copy to the stack.
+            const Vector<xdim> x = xhat0_;
+            const Vector<udim> u = uhat0_;
+            
+            Matrix<xdim, xdim> A; 
+            Matrix<xdim, udim> B;
+            linearize_dynamics(branch.dynamics, x, u, A, B);
+
+            const Matrix<xdim,xdim> &Vt1 = branch.Vs[1];
+            const Matrix<1,xdim> &Gt1 = branch.Gs[1];
+            weighted_inv_term.noalias() += p * (B.transpose()*(Vt1+LM)*B);
+            weighted_Kt_term.noalias()  += p * (B.transpose()*(Vt1+LM)*A);
+            weighted_kt_term.noalias()  += p * (B.transpose()*Gt1.transpose());
+
+            Matrix<xdim,xdim> Q; Matrix<udim,udim> R; Matrix<xdim,udim> P;
+            Vector<xdim> g_x; Vector<udim> g_u;
+            quadratize_cost(branch.cost, 0, x, u, Q, R, P, g_x, g_u);
+
+            wQ += p * Q;
+            wR += p * R;
+            wP += p * P;
+            wg_x += p * g_x;
+            wg_u += p * g_u;
+        }
+
+        const Matrix<udim,udim> inv_term = -1.0*(wR + weighted_inv_term).inverse();
+        K0_.noalias() = inv_term * (wP.transpose() + weighted_Kt_term);
+        k0_.noalias() = inv_term * (wg_u + weighted_kt_term);
+
+        // Copy the first timestep K to all the branches.
+        for (auto &branch : branches_)
+        {
+            branch.Ks[0] = K0_;
+            branch.ks[0] = k0_;
+            // Confirm that these are already equal across branches.
+            IS_TRUE(math::is_equal(branch.xhat[0], xhat0_));
+            IS_TRUE(math::is_equal(branch.uhat[0], uhat0_));
+        }
+    }
+
+    if (verbose)
+    {
+        SUCCESS("Converged after " << iter << " iterations.");
+    }
+}
+ 
+
+template<int xdim, int udim>
+inline void iLQRHindsightValueSolver<xdim,udim>::bellman_backup(
+        const int branch_num,
+        const int t,
+        const double mu, // Levenberg-Marquardt parameter
+        const Matrix<xdim,xdim> &Vt1, 
+        const Matrix<1,xdim> &Gt1, 
+        const double Wt1,
+        Matrix<xdim,xdim> &Vt, 
+        Matrix<1,xdim> &Gt,
+        double &Wt
+        )
+{
+    HindsightBranchValue<xdim,udim> &branch = branches_[branch_num];
+    const Vector<xdim> x = branch.xhat[t];
+    const Vector<udim> u = branch.uhat[t];
+
+    Matrix<xdim, xdim> A; 
+    Matrix<xdim, udim> B;
+    linearize_dynamics(branch.dynamics, x, u, A, B);
+
+    Matrix<xdim,xdim> Q;
+    Matrix<udim,udim> R;
+    Matrix<xdim,udim> P;
+    Vector<xdim> g_x;
+    Vector<udim> g_u;
+    quadratize_cost(branch.cost, t, x, u, Q, R, P, g_x, g_u);
+
+    // Levenberg-Marquardt parameter for damping. 
+    // ie. eigenvalue inflation matrix.
+    const Matrix<xdim, xdim> LM = mu * Matrix<xdim, xdim>::Identity();
+
+    const Matrix<udim, udim> inv_term 
+        = -1.0*(R + B.transpose()*(Vt1+LM)*B).inverse();
+
+    branch.Ks[t] = inv_term * (P.transpose() + B.transpose()*(Vt1+LM)*A); 
+    branch.ks[t] = inv_term * (g_u + B.transpose()*Gt1.transpose());
+    const Matrix<udim, xdim> &Kt = branch.Ks[t];
+    const Vector<udim> &kt = branch.ks[t];
+
+    const Matrix<xdim, xdim> tmp = (A + B*Kt);
+    Vt = Q + 2.0*(P*Kt) 
+        + Kt.transpose()*R*Kt + tmp.transpose()*Vt1*tmp;
+
+    Gt = kt.transpose()*P.transpose() 
+        + kt.transpose()*R*Kt + g_x.transpose() 
+        + g_u.transpose()*Kt + kt.transpose()*B.transpose()*Vt1*tmp + Gt1*tmp;
+   
+    // This does not affect the policy but is part of the value function.
+    const double c = branch.cost(x, u, t);
+    const Vector<xdim> tmp2 = B*kt;
+    Matrix<1,1> Wt_mat = Gt1*B*kt + 0.5*(tmp2.transpose()*Vt1*tmp2);
+    Wt = Wt_mat(0) + Wt1 + c;
+}
+
+template<int xdim, int udim>
+inline int iLQRHindsightValueSolver<xdim,udim>::timesteps() const
+{ 
+    IS_GREATER(branches_.size(), 0);
+    const size_t T = branches_[0].uhat.size(); 
+    // Confirm that all the required parts the same size
+    IS_EQUAL(T, branches_[0].ks.size());
+    IS_EQUAL(T, branches_[0].Ks.size());
+    IS_EQUAL(T+1, branches_[0].xhat.size());
+    return static_cast<int>(T); 
+}
+
+template<int xdim, int udim>
+inline void iLQRHindsightValueSolver<xdim,udim>::set_branch_probability(const int branch_num, const double probability)
+{
+    IS_BETWEEN_LOWER_INCLUSIVE(branch_num, 0, branches_.size());
+    IS_BETWEEN_INCLUSIVE(probability, 0, 1.0);
+    branches_[branch_num].probability = probability;
+}
+
+template<int xdim, int udim>
+inline double iLQRHindsightValueSolver<xdim,udim>::total_branch_probability()
+{
+    double total_prob = 0;
+    for (const auto &branch : branches_)
+    {
+        total_prob += branch.probability;
+    }
+    return total_prob;
+}
+
+template<int xdim, int udim>
+inline double iLQRHindsightValueSolver<xdim, udim>::compute_value(const int branch_num, const Vector<xdim> &xt, const int t) const
+{
+    IS_BETWEEN_LOWER_INCLUSIVE(branch_num, 0, branches_.size());
+    const int T = timesteps();
+    IS_BETWEEN_INCLUSIVE(t, 0, T+1);
+
+    const HindsightBranchValue<xdim,udim> &branch = branches_[branch_num];
+    // Value function is actually defined in terms of the state-difference 
+    // from the Taylor expansion point.
+    const Vector<xdim> zt = (xt - branch.xhat[t]);
+
+    // Compute the quadratic value function. 
+    const double Jt = zt.transpose() *  branch.Vs[t] * zt + branch.Gs[t] * zt + branch.Ws[t];
+    return Jt;
+}
+
+} // namespace ilqr
